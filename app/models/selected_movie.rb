771785a325f141ab5add7a58b7339ea1dcd81130
Remi Achji <achji.remi@gmail.com>
2021-02-26 19:09:14 +0400

    movies can be added to watchlist once per user

--- app/models/selected_movie.rb
+++ app/models/selected_movie.rb
@@ -1,4 +1,5 @@
 class SelectedMovie < ApplicationRecord
   belongs_to :watchlist
   belongs_to :movie
+  validates_uniqueness_of :watchlist_id, scope: :movie_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :watchlist
+  has_many :movies, through: :watchlist
 end
