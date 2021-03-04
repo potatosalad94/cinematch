bb10a2de7b83ba3ec68eaade122114f394b07121
Remi Achji <achji.remi@gmail.com>
2021-03-04 12:33:49 +0400

    added first and last name to user, update user registration and added photo to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # has_one :watchlist
+  has_one_attached :photo
   has_many :watchlists
   has_many :movies, through: :watchlists
 end
