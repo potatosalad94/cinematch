b4a218581e3385d395b4593f6a9ec58701404d2b
Remi Achji <achji.remi@gmail.com>
2021-03-07 13:10:40 +0400

    added show and index for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @friends = current_user.friends
+    @pending_requests = current_user.pending_requests
+    @friend_requests = current_user.received_requests
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
