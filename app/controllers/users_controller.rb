c68d1cea0dbc4c819481309c860d59e7c6a5ddb2
Remi Achji <achji.remi@gmail.com>
2021-03-07 17:58:01 +0400

    added pagy on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    # @users = User.all
+    @pagy, @users = pagy(User.all)
     @friends = current_user.friends
     @pending_requests = current_user.pending_requests
     @friend_requests = current_user.received_requests
