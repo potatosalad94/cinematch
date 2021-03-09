e033af8315cff059465214e8166f016c05d8599f
Remi Achji <achji.remi@gmail.com>
2021-03-09 17:54:58 +0400

    added search in watchlists index

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,7 +1,15 @@
 class Watchlist < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   belongs_to :movie
   # belongs_to :users, through: :watchlist
 
   validates_uniqueness_of :user_id, scope: :movie_id, message: "Movie is already in your watchlist!"
+  pg_search_scope :global_search,
+                  associated_against: {
+                    movie: [:title, :cast, :directors, :genres, :overview, :release_date]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
