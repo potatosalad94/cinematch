1940edaca13d57d5b85c111ddb85ba128a4b7950
Remi Achji <achji.remi@gmail.com>
2021-03-21 14:46:31 +0400

    added event creation form  with a list of current friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,8 @@ class User < ApplicationRecord
            through: :friend_request, source: :sent_by
 
   validates :email, uniqueness: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
