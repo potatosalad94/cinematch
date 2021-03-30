41e67903374afd96f08514179e3a8ae997f96cd0
Remi Achji <achji.remi@gmail.com>
2021-03-30 16:30:16 +0400

    getting back

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Creating 100 users...'
+User.create!(first_name: 'Remi', last_name: 'Achji', email: 'rem@gmail.com', password: '123456')
+User.create!(first_name: 'Ben', last_name: 'Merigot', email: 'ben@gmail.com', password: '123456')
 100.times do |i|
   User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.unique.email, password: '123456')
 end
